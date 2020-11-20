e0f71a6737fba483a025090665504d08627b79c6
Amneriis <46808792+Amneriis@users.noreply.github.com>
2020-11-20 17:02:06 +0100

    Préparation navbar

--- ScamBetting/resources/views/home.blade.php
+++ ScamBetting/resources/views/home.blade.php
@@ -1,44 +1,28 @@
 <html>
 
     <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/ressources/scss/app.scss" rel="stylesheet">
     </head>
 
     <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form>
-            </div>
+        <nav>
+            <p class="bg-gray-300">Famille, vie sociale, santé financière. Etes-vous prêt à tout miser ?
+                Pour être aidé, appelez le 09-74-75-13-13
+            </p>
+            <a>Accueil</a>
+            <a>Esport</a>
+            <a>Match en live</a>
+            <a>Se connecter</a>
+            <a>S'inscrire</a>
         </nav>
+
+    <footer class="bg-gray-300">
+        <p>Scam betting est un projet étudiant de paris esportifs fictif en ligne.</p>
+    </footer>
+
     </body>
 
 </html>
