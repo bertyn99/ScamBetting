e23dd851f8d1aad42376f7a4fde3b8cfc1f30747
bertyn99 <bboulikou@gmail.com>
2020-11-20 16:12:39 +0100

    ajout tailwindcss

--- ScamBetting/webpack.mix.js
+++ ScamBetting/webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,7 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    });
