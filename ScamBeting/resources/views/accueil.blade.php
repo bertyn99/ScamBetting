2712c1f4759c6d975fa63f01c72df806741bed5f
Amneriis <46808792+Amneriis@users.noreply.github.com>
2020-11-26 15:50:36 +0100

    Ajout html navbar

---
+++ ScamBeting/resources/views/accueil.blade.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Scam Betting</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+
+<body>
+    <p class="bg-gray-300 text-center">Famille, vie sociale, santé financière. Etes-vous prêt à tout miser ?
+Pour être aidé, appelez le 09-74-75-13-13
+</p>
+    <header>
+        <div class="flex">
+                <ul class="contents flex-row space-x-8">
+                    <li><a href="#" class="ml-8">Accueil</a></li>
+                    <li><a href="#">E-sports</a></li>
+                    <li><a href="#">Match en live</a></li>
+                </ul>
+        </div>
+            <div>
+                <ul class="flex flex-row-reverse space-x-4 space-x-reverse">
+                    <li><a href="#">Connexion</a></li>
+                    <li><a href="#">S'inscrire</a></li>
+                </ul>
+        </div>
+
+    </header>
+
+
+
+    <footer class="bg-gray-300 text-center">
+        <p>Scam betting est un projet étudiant de paris e-sportifs fictif en ligne.</p>
+    </footer>
+
+</body>
+
+</html>
