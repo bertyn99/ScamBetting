47a2b91a719fb485d6ef583c19394f7a92d82f25
Amneriis <46808792+Amneriis@users.noreply.github.com>
2020-11-27 16:14:19 +0100

    Ajout table jeu et equipe

---
+++ ScamBeting/database/migrations/2020_11_27_144014_create_equipe_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquipeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipe', function (Blueprint $table) {
+            $table->id();
+            $table->string(nom_equipe);
+            $table->foreignId('id_jeu');
+
+            $table->foreign('id_jeu')->references('id')->on('jeu');
+});
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipe');
+    }
+}
