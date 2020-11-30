4850c1e76f16082e1fb2abe5628a8eac88798991
Amneriis <46808792+Amneriis@users.noreply.github.com>
2020-11-30 14:30:06 +0100

    Ajout table Paris

---
+++ ScamBeting/database/migrations/2020_11_30_131407_create_paris_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paris', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_jeu');
+            $table->foreignId('id_equipe_1');
+            $table->foreignId('id_equipe_2');
+            $table->float('cote_1', 2, 2);
+            $table->float('cote_2', 2, 2);
+
+            $table->foreign('id_equipe_1')->references('id')->on('equipe');
+            $table->foreign('id_equipe_2')->references('id')->on('equipe');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paris');
+    }
+}
