e8a30c628a5fa18bdc18baf638593b5f6396e25d
bertyn99 <bboulikou@gmail.com>
2020-12-07 13:27:04 +0100

    ajout de route store et update

--- ScamBeting/routes/web.php
+++ ScamBeting/routes/web.php
@@ -28,7 +28,9 @@ Route::get('admin', [HomeController::class, 'admin'])->middleware('admin');
 # CRUD EQUIPE
 Route::get('admin/equipe', [EquipeController::class, 'index'])->middleware('admin');
 Route::get('admin/equipe/create', [EquipeController::class, 'create'])->middleware('admin');
+Route::post('admin/equipe', [EquipeController::class, 'store'])->middleware('admin');
 Route::put('admin/equipe', [EquipeController::class, 'edit'])->middleware('admin');
+Route::put('admin/equipe/{id}/update', [EquipeController::class, 'update'])->middleware('admin');
 Route::delete('admin/equipe', [EquipeController::class, 'destroy'])->middleware('admin');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
