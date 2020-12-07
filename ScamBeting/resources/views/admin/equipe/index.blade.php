35875699f6154edb3b5ef8192ae0175da519f7e1
bertyn99 <bboulikou@gmail.com>
2020-12-07 13:14:02 +0100

    ajout du equipe create

--- ScamBeting/resources/views/admin/equipe/index.blade.php
+++ ScamBeting/resources/views/admin/equipe/index.blade.php
@@ -1,6 +1,7 @@
 @extends('admin.admin')
 
 @section('equipe')
+<a href="{{ route('equipe.create') }}">ajout nouveau customer</a>
 <div class="h-full w-64 rounded bg-gray-400">
     @foreach ($equipe as $equipes)
         <div class="text-gray-100 p-2">
