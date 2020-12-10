cb6722bfac9b02ea1eadb79ec5ce805e99b9ca7c
bertyn99 <bboulikou@gmail.com>
2020-12-10 17:25:32 +0100

    ajout route paris

--- ScamBeting/routes/web.php
+++ ScamBeting/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\HomeController;
 use \App\Http\Controllers\EquipeController;
 use \App\Http\Controllers\JeuController;
+use \App\Http\Controllers\ParisController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,7 +33,7 @@ Route::get('admin/equipe/create', [EquipeController::class, 'create'])->middlewa
 Route::post('admin/equipe', [EquipeController::class, 'store'])->middleware('admin')->name("equipe.store");
 Route::get('admin/equipe/{id}/edit', [EquipeController::class, 'edit'])->middleware('admin')->name("equipe.edit");
 Route::put('admin/equipe/{id}/update', [EquipeController::class, 'update'])->middleware('admin')->name("equipe.update");
-Route::get('admin/equipe', [EquipeController::class, 'destroy'])->middleware('admin')->name("equipe.destroy");
+Route::delete('admin/equipe', [EquipeController::class, 'destroy'])->middleware('admin')->name("equipe.destroy");
 
 #CRUD JEU
 Route::get('admin/jeu', [JeuController::class, 'index'])->middleware('admin')->name("jeu.index");
@@ -41,5 +42,14 @@ Route::post('admin/jeu', [JeuController::class, 'store'])->middleware('admin')->
 Route::get('admin/jeu/{id}/edit', [JeuController::class, 'edit'])->middleware('admin')->name("jeu.edit");
 Route::put('admin/jeu/{id}/update', [JeuController::class, 'update'])->middleware('admin')->name("jeu.update");
 Route::get('admin/jeu/{id}/delete', [JeuController::class, 'destroy'])->middleware('admin')->name("jeu.destroy");
+
+#CRUD PARIS
+Route::get('admin/paris', [ParisController::class, 'index'])->middleware('admin')->name("bet.index");
+Route::get('admin/paris/create', [ParisController::class, 'create'])->middleware('admin')->name("bet.create");
+Route::post('admin/paris', [ParisController::class, 'store'])->middleware('admin')->name("bet.store");
+Route::get('admin/paris/{id}/edit', [ParisController::class, 'edit'])->middleware('admin')->name("bet.edit");
+Route::put('admin/paris/{id}/update', [ParisController::class, 'update'])->middleware('admin')->name("bet.update");
+Route::get('admin/paris/{id}/delete', [ParisController::class, 'destroy'])->middleware('admin')->name("bet.destroy");
+
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
