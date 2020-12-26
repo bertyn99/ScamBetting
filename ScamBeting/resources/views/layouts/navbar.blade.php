639d7f443afbdec22f8b0e69e99f7ac46a1bab54
bertyn99 <bboulikou@gmail.com>
2020-12-26 10:28:04 +0100

    mis een commentaire un a du dropdown

--- ScamBeting/resources/views/layouts/navbar.blade.php
+++ ScamBeting/resources/views/layouts/navbar.blade.php
@@ -82,7 +82,7 @@
                                 <a href="{{url('home')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Dashboard</a>
                                 @endif
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Settings</a>
-                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">{{ Auth::user()->name }} {{ Auth::user()->isAdmin }}</a>
+                               {{--  <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">{{ Auth::user()->name }} {{ Auth::user()->isAdmin }}</a> --}}
                                 <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                                     {{ csrf_field() }}
