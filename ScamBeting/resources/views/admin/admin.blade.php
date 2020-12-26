a004e17e1bfcc5decc1d8d500328739596f97231
bertyn <bertynboulikou@gmail.com>
2020-12-26 15:52:28 +0100

    nouveau index

--- ScamBeting/resources/views/admin/admin.blade.php
+++ ScamBeting/resources/views/admin/admin.blade.php
@@ -4,32 +4,32 @@
 <section>
     <div class="container md:mx-auto">
         <div class="p-6 bg-gray-50 flex flex-col ">
-        <nav class="text-white text-base font-semibold  flex flex-row w-full justify-center text-center">
-            <div class="p-2 mr-1 rounded-t w-24 bg-blue-600 hover:">
-                <a href="{{route('jeu.index')}}">Jeu</a>   
-            </div>
-            <div class="p-2 mr-1 w-24 rounded-t bg-blue-600">
-                <a href="{{route('equipe.index')}}">Equipe</a>         
-            </div>
-            <div class="p-2 mr-1 w-24 rounded-t bg-blue-600">
-                <a href="{{route('bet.index')}}">Paris</a>
-            </div>
-        </nav>  
-        <div class="bg-white w-5/6 mx-auto">
+            <nav class="text-white text-base font-semibold  flex flex-row w-full justify-center text-center">
+                <div class="p-2 mr-1 rounded-t w-24 bg-blue-600 hover:">
+                    <a href="{{route('jeu.index')}}">Jeu</a>
+                </div>
+                <div class="p-2 mr-1 w-24 rounded-t bg-blue-600">
+                    <a href="{{route('equipe.index')}}">Equipe</a>
+                </div>
+                <div class="p-2 mr-1 w-24 rounded-t bg-blue-600">
+                    <a href="{{route('bet.index')}}">Paris</a>
+                </div>
+            </nav>
+            <div class="bg-white w-full mx-auto flex flex-col items-center text-center p-2 rounded">
                 @yield('equipe')
 
-            @yield('jeu')
-            @yield('bet')
+                @yield('jeu')
+                @yield('bet')
+            </div>
         </div>
+
     </div>
-       
-    </div>
-    
+
 </section>
 
-           
+
 </div>
 
-    
+
 
 @endsection
--- ScamBeting/resources/views/admin/jeu/index.blade.php
+++ ScamBeting/resources/views/admin/jeu/index.blade.php
@@ -1,16 +1,37 @@
 @extends('admin.admin')
 
 @section('jeu')
-<h1>Jeux</h1>
-<a href="{{ route('jeu.create') }}">ajout nouveau jeu</a>
+
+
 <div class="h-full w-64 rounded bg-gray-400">
     @foreach ($jeux as $jeu)
     <div class="text-gray-100 p-2">
-       <a href="{{ route('jeu.edit',['id'=>$jeu->id]) }}">{{$jeu->nom_jeu}}</a>
-       <a href="{{ route('jeu.destroy',$jeu->id) }}" class="text-red-400" >X</a>
+        <a href="{{ route('jeu.edit',['id'=>$jeu->id]) }}">{{$jeu->nom_jeu}}</a>
+        <a href="{{ route('jeu.destroy',$jeu->id) }}" class="text-red-400">X</a>
     </div>
     @endforeach
 </div>
 
+<div class=" w-full rounded ">
+    <h3 class="font-bold text-center">Liste des Equipes</h3>
 
+    <!-- <div class="w-1/6 m-1"><a href="{{ route('jeu.create') }}" class=" bg-blue-600 p-2 rounded text-md font-bold text-white">Ajout Jeu</a></div> -->
+
+    <div class="bg-gray-200 m-2  rounded ">
+        @foreach ($jeux as $jeu)
+        <div class="text-gray-500 font-bold p-2 border-b flex flex-row text-gray-300 font-bold py-4">
+            <div class="w-8/12">
+                <a href="{{ route('jeu.edit',['id'=>$jeu->id]) }}">{{$equipe->nom_jeu}}</a>
+            </div>
+
+            <div class="w-4/12"> <a href="{{ route('equipe.destroy',$equipe->id) }}" class="w-24 p-1 mx-1 bg-green-400 text-white rounded ">
+                    Modifier
+                </a><a href="{{ route('jeu.destroy',$jeu->id) }}" class="w-24 p-1 bg-red-500 text-white rounded">
+                    Supprimer
+                </a></div>
+        </div>
+        @endforeach
+    </div>
+
+</div>
 @endsection
