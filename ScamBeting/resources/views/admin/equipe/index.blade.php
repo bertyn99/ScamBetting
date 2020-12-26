e654b33d654e2e9e7a46d6235726e143d8ab6eb2
bertyn <bertynboulikou@gmail.com>
2020-12-26 15:52:42 +0100

    add middleware in controller

--- ScamBeting/resources/views/admin/equipe/index.blade.php
+++ ScamBeting/resources/views/admin/equipe/index.blade.php
@@ -1,16 +1,28 @@
 @extends('admin.admin')
 
 @section('equipe')
-<h1> Equipes</h1>
-<a href="{{ route('equipe.create') }}">ajout nouveau customer</a>
-<h2>Liste des Equipes</h2>
-<div class="h-full w-64 rounded bg-gray-400">
-    @foreach ($equipes as $equipe)
-    <div class="text-gray-100 p-2">
-       <a href="{{ route('equipe.edit',['id'=>$equipe->id]) }}">{{$equipe->nom_equipe}}</a>
-       <a href="{{ route('equipe.destroy',$equipe->id) }}" class="text-red-400" >X</a>
+
+<div class=" w-full rounded ">
+    <h3 class="font-bold text-center">Liste des Equipes</h3>
+
+    <!-- <div class="w-1/6 m-1"><a href="{{ route('equipe.create') }}" class=" bg-blue-600 p-2 rounded text-md font-bold text-white">Ajout Equipe</a></div> -->
+
+    <div class="bg-gray-200 m-2  rounded ">
+        @foreach ($equipes as $equipe)
+        <div class="text-gray-500 font-bold p-2 border-b flex flex-row text-gray-300 font-bold py-4">
+            <div class="w-8/12">
+                <a href="{{ route('equipe.edit',['id'=>$equipe->id]) }}">{{$equipe->nom_equipe}}</a>
+            </div>
+
+            <div class="w-4/12"> <a href="{{ route('equipe.destroy',$equipe->id) }}" class="w-24 p-1 mx-1 bg-green-400 text-white rounded ">
+                    Modifier
+                </a><a href="{{ route('equipe.destroy',$equipe->id) }}" class="w-24 p-1 bg-red-500 text-white rounded">
+                    Supprimer
+                </a></div>
+        </div>
+        @endforeach
     </div>
-    @endforeach
+
 </div>
 
 
