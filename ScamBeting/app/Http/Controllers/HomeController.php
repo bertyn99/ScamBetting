dcef685cac0d6c679cd0e61ac4d08ff8b4b565bb
bertyn99 <bboulikou@gmail.com>
2020-12-27 22:09:10 +0100

    ajout deposit et witdraw

--- ScamBeting/app/Http/Controllers/HomeController.php
+++ ScamBeting/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserBet;
+
 
 class HomeController extends Controller
 {
@@ -24,5 +26,23 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    public function balance()
+    {
+        $bets = UserBet::where('id', auth()->user()->id)->get();
+
+        /*  Auth::user()->balance */
+    }
+
+    public function deposit()
+    {
+    }
+    public function withdraw(Request $request)
+    {
+
+        if ($request->amount > 0 && $request->amount <= auth()->user()->balance) {
+            auth()->user()->balance -= $request->amount;
+        }
+        return redirect()->route('dashboard.index');
     }
 }
