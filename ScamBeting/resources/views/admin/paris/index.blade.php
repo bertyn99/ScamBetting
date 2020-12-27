f53195ab00061b12637a08f214af4856bee2d6e5
bertyn99 <bboulikou@gmail.com>
2020-12-27 22:49:12 +0100

    list paris

--- ScamBeting/resources/views/admin/paris/index.blade.php
+++ ScamBeting/resources/views/admin/paris/index.blade.php
@@ -1,13 +1,20 @@
 @extends('admin.admin')
 
 @section('bet')
-<h1>Paris</h1>
 <a href="{{ route('bet.create') }}">ajout nouveau paris</a>
-<div class="h-full w-64 rounded bg-gray-400">
+
+<div class="bg-gray-200 m-2  rounded ">
     @foreach ($bets as $bet)
-    <div class="text-gray-100 p-2">
-       <a href="{{ route('bet.edit',['id'=>$bet->id]) }}">{{$bet->id}}</a>
-       <a href="{{ route('bet.destroy',$bet->id) }}" class="text-red-400" >X</a>
+    <div class="text-gray-500 font-bold p-2 border-b flex flex-row text-gray-300 font-bold py-4">
+        <div class="w-8/12">
+            <a href="{{ route('bet.edit',['id'=>$bet->id]) }}">{{$bet->id_equipe_1}} vs {{$bet->id_equipe_2}}</a>
+        </div>
+
+        <div class="w-4/12"> <a href="{{ route('bet.update',$bet->id) }}" class="w-24 p-1 mx-1 bg-green-400 text-white rounded ">
+                Modifier
+            </a><a href="{{ route('bet.destroy',$bet->id) }}" class="w-24 p-1 bg-red-500 text-white rounded">
+                Supprimer
+            </a></div>
     </div>
     @endforeach
 </div>
