57556ff8a7393016f15933449cedc79daaa4e45d
bertyn99 <bboulikou@gmail.com>
2020-12-28 21:50:22 +0100

    ajout du slug dan sle Jeuseeder

--- ScamBeting/database/seeders/JeuSeeder.php
+++ ScamBeting/database/seeders/JeuSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Jeu;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use League\CommonMark\Normalizer\SlugNormalizer;
 
 class JeuSeeder extends Seeder
 {
@@ -15,7 +16,7 @@ class JeuSeeder extends Seeder
      */
     public function run()
     {
-        $jeux = [['nom_jeu' => "Heartstone"], ['nom_jeu' => "Counter-Strike"], ['nom_jeu' => "League of Legends"], ['nom_jeu' => "Valorant"], ['nom_jeu' => "Rainbow 6 Siege"]];
+        $jeux = [['nom_jeu' => "Heartstone", 'slug' => "heartstone"], ['nom_jeu' => "Counter-Strike", 'slug' => "counter-strike"], ['nom_jeu' => "League of Legends", 'slug' => "league-of-legends"], ['nom_jeu' => "Valorant", 'slug' => "valorant"], ['nom_jeu' => "Rainbow 6 Siege", 'slug' => "rainbow-6-siege"]];
         foreach ($jeux as $jeu) {
             Jeu::create($jeu);
         }
