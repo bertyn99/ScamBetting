4c0d76917817df1b2597a591854ca816c707e4fb
bertyn99 <bboulikou@gmail.com>
2020-12-29 11:00:19 +0100

    modif css form

--- ScamBeting/resources/views/admin/jeu/edit.blade.php
+++ ScamBeting/resources/views/admin/jeu/edit.blade.php
@@ -2,12 +2,14 @@
 
 @section('jeu')
 <div class="flex items-center h-screen w-full bg-teal-lighter">
-    <div class="w-full bg-white rounded shadow-lg p-8 m-4">
-        <form action={{ route('jeu.update',['id'=>$jeu->id]) }} class="mb-6" method="post">
+    <div class="w-full bg-white rounded shadow-lg p-8 m-4 md:max-w-sm md:mx-auto">
+        <h3 class="block w-full text-center text-gray-600 mb-6">Modifier un jeu</h3>
+        <form action={{ route('jeu.update',['id'=>$jeu->id]) }} class="mb-4 md:flex md:flex-wrap md:justify-between"
+            method="post">
             @csrf
             @method('PUT')
             <div class="flex flex-col mb-4">
-                <label class="mb-2 uppercase font-bold text-lg text-grey-darkest" for="nom_equipe">Nom du Jeu</label>
+                <label class="field-label" for="nom_equipe">Nom du Jeu</label>
                 <input type="text" id="nom-form" name="nom_equipe" placeholder="nomEquipe" value="{{$jeu->nom_jeu}}">
             </div>
 
@@ -15,7 +17,7 @@
                 type="submit">Update</button>
 
         </form>
-        <a class="block w-full text-center no-underline text-sm text-grey-dark hover:text-grey-darker"
+        <a class="block w-full text-center no-underline text-sm text-gray-400 hover:text-gray-600"
             href="{{ route('jeu.index') }}"> Liste Jeu</a>
     </div>
 </div>
