2edfcd04eb5e3a26a53736f8f56a32451c3fba73
bertyn99 <bboulikou@gmail.com>
2020-12-30 16:53:46 +0100

    nouvelle modif

--- ScamBeting/app/Http/Controllers/AdminController.php
+++ ScamBeting/app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{User};
 
 class AdminController extends Controller
 {
@@ -14,5 +15,24 @@ class AdminController extends Controller
     public function index()
     {
         return view('admin.admin');
+    }
+
+    public function user()
+    {
+        $users = User::all();
+        return view('admin.admin', compact('users'));
+    }
+
+    public function edit()
+    {
+    }
+    public function update()
+    {
+    }
+    public function store()
+    {
+    }
+    public function show()
+    {
     }
 }
--- ScamBeting/resources/views/admin/admin.blade.php
+++ ScamBeting/resources/views/admin/admin.blade.php
@@ -1,9 +1,12 @@
 @extends('../layouts.app')
 
 @section('content')
-<section>
+<section class="py-10 ">
+    <div class="container mx-auto px-4">
+        <div class=" flex flex-col break-words bg-white w-full mb-6 shadow-xl rounded-lg ">
+            {{-- <section>
     <div class="container md:mx-auto">
-        <div class="p-6 bg-gray-50 flex flex-col ">
+        <div class="p-6 bg-gray-50 flex flex-col "> --}}
             <nav class="text-white text-base font-semibold  flex flex-row w-full justify-center text-center">
                 <div class="p-2 mr-1 rounded-t w-24 bg-blue-600 hover:">
                     <a href="{{route('jeu.index')}}">Jeu</a>
@@ -16,6 +19,7 @@
                 </div>
             </nav>
             <div class="bg-white w-full mx-auto flex flex-col items-center text-center p-2 rounded">
+                @yield('user')
                 @yield('equipe')
                 @yield('jeu')
                 @yield('bet')
@@ -25,9 +29,6 @@
     </div>
 
 </section>
-
-
-</div>
 
 
 
