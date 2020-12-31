50e0e9c948f9022ad57399eb7208b1c16d346485
bertyn99 <bboulikou@gmail.com>
2020-12-31 18:49:00 +0100

    ajout des paris sur la page d"accueil

--- ScamBeting/resources/views/layouts/paris/bet.blade.php
+++ ScamBeting/resources/views/layouts/paris/bet.blade.php
@@ -1,15 +1,16 @@
 <section>
     <div class="grid grid-cols-6 gap-2 py-2 px-5">
-    betee
+
         @foreach ($bets as $bet)
         <div class=" bg-white rounded flex flex-row justify-center just p-2 col-span-2 shadow">
             <div class="equipe text-center">
                 <div><img src="" alt=""></div>
                 <div class="text-center p-1">
-                    <h4 class="font-bold text-gray-800"> Equipe title</h4>
+                    <h4 class="font-bold text-gray-800"> {{$bet->equipes[0]->nom_equipe }}</h4>
                     <div class="mt-2">
                         <span>Cote</span>
-                        <button class="w-12 p-1 bg-blue-300 hover:bg-blue-500">5.2</button>
+                        <button
+                            class="w-12 p-1 bg-blue-300 hover:bg-blue-500">{{$bet->equipes[0]->pivot->cote}}</button>
                     </div>
                 </div>
             </div>
@@ -19,10 +20,11 @@
             <div class="equipe">
                 <div><img src="..\resources\img\Fnatic-Logo-Orange-RGB.svg" alt=""></div>
                 <div class="text-center p-1">
-                    <h4 class="font-bold text-gray-800"> Equipe title</h4>
+                    <h4 class="font-bold text-gray-800"> {{$bet->equipes[1]->nom_equipe }}</h4>
                     <div class="mt-2">
                         <span>Cote</span>
-                        <button class="w-12 p-1 bg-blue-300 hover:bg-blue-500">5.2</button>
+                        <button
+                            class="w-12 p-1 bg-blue-300 hover:bg-blue-500">{{$bet->equipes[1]->pivot->cote}}</button>
                     </div>
                 </div>
             </div>
